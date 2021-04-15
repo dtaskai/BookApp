ad4bfdd39ad91c56dc13c0ede155ab559dbf1209
Táskai Dominik <dtaskai@protonmail.com>
2021-04-15 17:55:10 +0200

    Removed unnecessary Control class usage (now deprecated)

--- src/main/java/hu/mik/pte/prog4/MainApplication.java
+++ src/main/java/hu/mik/pte/prog4/MainApplication.java
@@ -1,11 +1,7 @@
 package hu.mik.pte.prog4;
 
-import hu.mik.pte.prog4.controller.Control;
-import hu.mik.pte.prog4.service.BookService;
-
 public class MainApplication {
     public static void main(String[] args) {
-        Control control = new Control(new BookService());
-        control.start();
+
     }
 }
