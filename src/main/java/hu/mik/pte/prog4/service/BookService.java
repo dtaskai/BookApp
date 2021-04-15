6b899213463601e70536457b2b2ef480bf4db705
Táskai Dominik <dtaskai@protonmail.com>
2021-04-15 17:52:43 +0200

    Changed logger to Log4j2

--- src/main/java/hu/mik/pte/prog4/service/BookService.java
+++ src/main/java/hu/mik/pte/prog4/service/BookService.java
@@ -2,19 +2,15 @@ package hu.mik.pte.prog4.service;
 
 import hu.mik.pte.prog4.model.Book;
 import lombok.extern.log4j.Log4j2;
-import java.util.logging.Logger;
 
 
 @Log4j2
 public class BookService implements Service<Book>{
 
-
-    Logger logger = Logger.getLogger(getClass().getName());
-
     @Override
     public void changeCompletion(Book book) {
         book.setCompleted(!book.isCompleted());
-        logger.info(book + " marked as " + book.isCompleted());
+        log.info(book + " marked as " + book.isCompleted());
     }
 
     @Override
