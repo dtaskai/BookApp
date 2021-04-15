6a8ef43d5dbd1b126421feae31fed8bde042b081
Táskai Dominik <dtaskai@protonmail.com>
2021-04-15 17:43:52 +0200

    Added @ApplicationPath with API versioning

---
+++ src/main/java/hu/mik/pte/prog4/rest/Rest.java
@@ -0,0 +1,8 @@
+package hu.mik.pte.prog4.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class Rest extends Application {
+}
