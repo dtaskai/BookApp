c094a3e9487983ae2314fa8f2869537e75684968
Táskai Dominik <dtaskai@protonmail.com>
2021-04-23 19:36:30 +0200

    Added Role and User classes

---
+++ src/main/java/hu/mik/pte/prog4/entity/Role.java
@@ -0,0 +1,10 @@
+package hu.mik.pte.prog4.entity;
+
+import lombok.Data;
+
+@Data
+public class Role {
+    private Long id;
+    private String code;
+    private String description;
+}
---
+++ src/main/java/hu/mik/pte/prog4/entity/User.java
@@ -0,0 +1,10 @@
+package hu.mik.pte.prog4.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+}
