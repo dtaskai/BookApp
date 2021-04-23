54f6d7712f0e487066b1465fdafaad86490365df
Táskai Dominik <dtaskai@protonmail.com>
2021-04-23 19:42:29 +0200

    Updated book service ISBN check regex

--- src/main/java/hu/mik/pte/prog4/service/BookService.java
+++ src/main/java/hu/mik/pte/prog4/service/BookService.java
@@ -1,6 +1,6 @@
 package hu.mik.pte.prog4.service;
 
-import hu.mik.pte.prog4.model.Book;
+import hu.mik.pte.prog4.entity.Book;
 import lombok.extern.log4j.Log4j2;
 
 
@@ -15,9 +15,7 @@ public class BookService implements Service<Book>{
 
     @Override
     public boolean isValidISBN(Book book) {
-        return book.getISBN().matches("^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})\n" +
-                "[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)\n" +
-                "(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$");
+        return book.getISBN().matches("^(?:ISBN(?:-13)?:?\\ )?(?=[0-9]{13}$|(?=(?:[0-9]+[-\\ ]){4})[-\\ 0-9]{17}$)97[89][-\\ ]?[0-9]{1,5}[-\\ ]?[0-9]+[-\\ ]?[0-9]+[-\\ ]?[0-9]$");
     }
 
 
--- src/main/java/hu/mik/pte/prog4/service/Service.java
+++ src/main/java/hu/mik/pte/prog4/service/Service.java
@@ -1,6 +1,6 @@
 package hu.mik.pte.prog4.service;
 
-import hu.mik.pte.prog4.model.Book;
+import hu.mik.pte.prog4.entity.Book;
 
 public interface Service<T extends Book> {
 
