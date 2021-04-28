6d0c75a084ed525f1361d044d7dc406242c599c7
Táskai Dominik <dtaskai@protonmail.com>
2021-04-28 15:23:16 +0200

    Improved logout

--- src/main/java/hu/mik/pte/prog4/servlet/LogoutServlet.java
+++ src/main/java/hu/mik/pte/prog4/servlet/LogoutServlet.java
@@ -4,13 +4,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession(false).invalidate();
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        } else {
+            req.getSession(false).invalidate();
+        }
         req.logout();
-        resp.sendRedirect("LoginPage.jsp");
+
+        resp.sendRedirect(req.getServletContext().getContextPath());
     }
 }
