f29b227d02c2600d0688f17be15e44d63b0c21dd
Táskai Dominik <dtaskai@protonmail.com>
2021-05-12 12:32:10 +0200

    New services added based on the repository, also implemented in the BookService class

--- src/main/java/hu/mik/pte/prog4/service/BookService.java
+++ src/main/java/hu/mik/pte/prog4/service/BookService.java
@@ -1,11 +1,45 @@
 package hu.mik.pte.prog4.service;
 
 import hu.mik.pte.prog4.entity.Book;
+import hu.mik.pte.prog4.repository.BookRepository;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 
 @Log4j2
-public class BookService implements Service<Book>{
+public class BookService implements Service<Book> {
+
+    private final BookRepository bookRepository;
+
+    public BookService() {
+        this.bookRepository = new BookRepository();
+    }
+
+    @Override
+    public List<Book> listAll() {
+        return bookRepository.listAll();
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.saveBook(book);
+    }
+
+    @Override
+    public Book update(Book book) {
+        return bookRepository.updateBook(book);
+    }
+
+    @Override
+    public Book findById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return bookRepository.delete(id);
+    }
 
     @Override
     public void changeCompletion(Book book) {
--- src/main/java/hu/mik/pte/prog4/service/Service.java
+++ src/main/java/hu/mik/pte/prog4/service/Service.java
@@ -2,7 +2,19 @@ package hu.mik.pte.prog4.service;
 
 import hu.mik.pte.prog4.entity.Book;
 
+import java.util.List;
+
 public interface Service<T extends Book> {
+
+    List<T> listAll();
+
+    T save(T book);
+
+    T update(T book);
+
+    T findById(Long id);
+
+    boolean delete(Long id);
 
     void changeCompletion(T book);
 
