ef3bf9bd251962e272927764a72ce8c948de0e7f
Táskai Dominik <dtaskai@protonmail.com>
2021-05-12 12:32:33 +0200

    Refactored the class to use the BookService class instead of BookRepository

--- src/main/java/hu/mik/pte/prog4/rest/BookRestController.java
+++ src/main/java/hu/mik/pte/prog4/rest/BookRestController.java
@@ -1,7 +1,7 @@
 package hu.mik.pte.prog4.rest;
 
 import hu.mik.pte.prog4.entity.Book;
-import hu.mik.pte.prog4.repository.BookRepository;
+import hu.mik.pte.prog4.service.BookService;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -9,49 +9,49 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 
-@Path("/")
+@Path("/book")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class BookRestController {
 
-    private final BookRepository bookRepository;
+    private final BookService bookService;
 
     public BookRestController() {
-        this.bookRepository = new BookRepository();
+        this.bookService = new BookService();
     }
 
     @GET
     @Path("/list")
     public Response listAll() {
-        List<Book> book = bookRepository.listAll();
+        List<Book> book = bookService.listAll();
         return Response.status(200).entity(book).build();
     }
 
     @GET
     @Path("/{id}")
     public Response findById(@PathParam("id") Long id) {
-        Book book = bookRepository.findById(id);
+        Book book = bookService.findById(id);
         return Response.ok().entity(book).build();
     }
 
     @POST
     @Path("/add")
     public Response add(Book book) {
-        Book addedBook = bookRepository.saveBook(book);
+        Book addedBook = bookService.save(book);
         return Response.ok().entity(addedBook).build();
     }
 
     @PUT
     @Path("/{id}")
     public Response update(@PathParam("id") Long id, Book bookToUpdate) {
-        Book updatedBook = bookRepository.updateBook(bookToUpdate);
+        Book updatedBook = bookService.update(bookToUpdate);
         return Response.ok().entity(updatedBook).build();
     }
 
     @DELETE
     @Path("/{id}")
     public Response delete(@PathParam("id") Long id) {
-        bookRepository.delete(id);
+        bookService.delete(id);
         return Response.status(204).build();
     }
 }
